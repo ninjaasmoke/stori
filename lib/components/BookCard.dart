426c26918fa6e0b358ee7adbcabcee25f1eb171b
ninjaasmoke <nithins674@gmail.com>
2021-08-01 13:27:33 +0530

    modular cached image

--- lib/components/BookCard.dart
+++ lib/components/BookCard.dart
@@ -1,45 +1,11 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:stori/constants.dart';
+import 'package:stori/components/CustomCachedImage.dart';
 import 'package:stori/models/BookModel.dart';
 
 Widget bookCard(BookModel book) {
-  return CachedNetworkImage(
-    imageUrl: book.imageUrl.isEmpty ? IMAGE_NOT_FOUND_URL : book.imageUrl,
-    placeholder: (context, url) => Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(6.0),
-          color: searchBarColor,
-        ),
-        child: Center(
-          child: CircularProgressIndicator(
-            valueColor: AlwaysStoppedAnimation<Color>(tileColor),
-          ),
-        )),
-    imageBuilder: (context, imageProvider) => Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(6.0),
-          image: DecorationImage(
-            image: imageProvider,
-            fit: BoxFit.cover,
-            colorFilter: ColorFilter.mode(
-                book.imageUrl.isEmpty ? Colors.black45 : Colors.transparent,
-                BlendMode.multiply),
-          ),
-          color: searchBarColor,
-        ),
-        alignment: Alignment.bottomLeft,
-        padding: EdgeInsets.all(8.0),
-        child: Text(
-          book.imageUrl.isEmpty ? book.title : "",
-          style: TextStyle(
-            color: primaryTextColor,
-          ),
-        )),
-    errorWidget: (context, url, error) => Container(
-        child: Icon(
-      Icons.emoji_emotions,
-      color: accentcolor,
-    )),
+  return customCachedNetworkImage(
+    url: book.imageUrl,
+    title: book.title,
   );
 }
--- lib/pages/Profile.dart
+++ lib/pages/Profile.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:stori/components/CustomCachedImage.dart';
 import 'package:stori/constants.dart';
 import 'package:stori/logic/UserBloc.dart';
+import 'package:stori/models/UserModel.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -12,6 +14,7 @@ class ProfilePage extends StatelessWidget {
       builder: (context, state) {
         return Scaffold(
           appBar: _appBar(),
+          body: state is LoggedInUserState ? _body(state.user) : Center(),
         );
       },
     );
@@ -22,6 +25,44 @@ class ProfilePage extends StatelessWidget {
       backgroundColor: appBarBGColor,
       automaticallyImplyLeading: true,
       title: Text('Profile & More'),
+    );
+  }
+
+  Widget _body(AppUser user) {
+    return Column(
+      children: [_profileBanner(user.photoURL, user.username)],
+    );
+  }
+
+  Widget _profileBanner(String? userPhoto, String? userName) {
+    return Container(
+      height: 200,
+      padding: const EdgeInsets.all(10),
+      color: scaffoldBGColor,
+      alignment: Alignment.center,
+      child: Column(
+        children: [
+          Container(
+            width: 100,
+            height: 100,
+            child: Center(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: customCachedNetworkImage(url: userPhoto!),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 12.0,
+          ),
+          Text(
+            userName!,
+            style: TextStyle(
+              color: tertiaryTextColor,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
